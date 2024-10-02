18034a8ed61fa56c4e20c7b957fd953c1adc7a9e
Brandon Kim <brandonkim@Brandons-Air-86.lan>
2024-10-01 20:35:39 -0700

    initial commit

--- some_heap.c
+++ some_heap.c
@@ -1,26 +1,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#include "heap.h"
+#include "some_heap.h"
 
 #define KEY_NOT_PRESENT -1
 
 heap_t *heap_create(int capacity) {
+    heap_t *heap = (heap_t *)malloc(sizeof(heap_t));
+    heap->data = (heap_node_t *)malloc(sizeof(heap_node_t) * capacity);
+    heap->size = 0;
+    heap->capacity = capacity;
+    return heap;
 }
 
 void heap_free(heap_t *heap) {
+    free(heap->data);
+    heap->data = NULL;
+    free(heap);
 }
 
 unsigned int heap_size(heap_t *heap) { return heap->size; }
 
-unsigned int heap_parent(unsigned int index) {  }
+unsigned int heap_parent(unsigned int index) { return (index - 1) / 2; }
 
-unsigned int heap_left_child(unsigned int index) { }
+unsigned int heap_left_child(unsigned int index) { return (index * 2) + 1; }
 
-unsigned int heap_right_child(unsigned int index) {  }
+unsigned int heap_right_child(unsigned int index) { return (index * 2) + 2; }
 
-unsigned int heap_level(unsigned int index) {}
+unsigned int heap_level(unsigned int index) {
+    return (unsigned int)log2(index + 1); // check this
+}
 
 void heap_print(heap_t *heap) {
     for (int ix = 0; ix < heap_size(heap); ix++) {
@@ -31,12 +42,42 @@ void heap_print(heap_t *heap) {
 }
 
 void heap_swap(heap_t *heap, int index1, int index2) {
+    heap_node_t temp = heap->data[index1];
+    heap->data[index1] = heap->data[index2];
+    heap->data[index2] = temp;
 }
 
 void heap_bubble_up(heap_t *heap, int index) {
+    while (heap_level(index) > 0) {
+        int parent = heap_parent(index);
+
+        if (heap->data[index].key > heap->data[parent].key) {
+            break;
+        }
+
+        heap_swap(heap, index, parent);
+        index = parent;
+    }
 }
 
 void heap_bubble_down(heap_t *heap, int index) {
+    while (heap_left_child(index) < heap_size(heap)) {
+        int left_child = heap_left_child(index);
+        int right_child = heap_right_child(index);
+        int smallest = left_child;
+
+        if (right_child < heap_size(heap) &&
+            heap->data[right_child].key < heap->data[left_child].key) {
+                smallest = right_child;
+        }
+
+        if (heap->data[index].key < heap->data[smallest].key) {
+            break;
+        }
+
+        heap_swap(heap, index, smallest);
+        index = smallest;
+    }
 }
 
 void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
@@ -54,7 +95,7 @@ void heap_insert(heap_t *heap, heap_key_t key, heap_value_t data) {
 
 heap_value_t heap_remove_min(heap_t *heap) {
     if (heap_size(heap) == 0) {
-        return NULL;
+        return (heap_value_t){.as_int = KEY_NOT_PRESENT};
     }
 
     heap_value_t min = heap->data[0].value;
--- some_heap.h
+++ some_heap.h
@@ -1,12 +1,16 @@
 
-#ifndef HEAP_H
-#define HEAP_H
+#ifndef SOME_HEAP_H
+#define SOME_HEAP_H
 
 // a min heap
 
 typedef unsigned long long heap_key_t;
-typedef void* heap_value_t;
 #define HEAP_KEY_FORMAT "%llu"
+
+typedef union heap_val {
+    unsigned long long as_int;
+    void *as_ptr;
+} heap_value_t;
 
 typedef struct {
     heap_key_t key;
